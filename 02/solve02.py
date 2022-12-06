6f85c299f5bc8715b2f9cbb560658c6d26179570
René Panák <rene.panak@seznam.cz>
2022-12-06 17:29:03 +0100

    solve 02-2

--- 02/solve02.py
+++ 02/solve02.py
@@ -9,6 +9,13 @@ import utils
 
 ISSUE=2
 
+# if you play key and opponent plays value, you win
+RULES = {
+	3:2,
+	2:1,
+	1:3
+}
+
 def rock_paper_scissors(player1:int, player2:int) -> int:
 	'''
 	in: 1=rock, 2=paper, 3=scissors
@@ -16,12 +23,7 @@ def rock_paper_scissors(player1:int, player2:int) -> int:
 	'''
 	if player1==player2:
 		return 0
-	rules = {
-		3:2,
-		2:1,
-		1:3
-	}
-	if rules[player1]==player2:
+	if RULES[player1]==player2:
 		return -1
 	else:
 		return 1
@@ -77,9 +79,34 @@ def solve_part_2(demo:bool) -> str:
 	score = 0
 	for gr in rounds:
 		# gr as "game round"
-		score += get_round_score2(gr[0], gr[1])
+		values_opp = " ABC"
+		turn_opp = values_opp.find(gr[0])
 
-	answer = None
+		outcome = 0
+		turn_you = 0
+		behav = gr[1]
+		opp_wins_on = RULES[turn_opp]
+		if behav == 'Y':
+			# draw
+			turn_you = turn_opp
+			outcome = 0
+		elif behav == 'X':
+			turn_you = opp_wins_on
+			outcome = -1
+		else:
+			# ~ print(turn_opp, opp_wins_on)
+			rk = set(RULES.keys())
+			rk.remove(turn_opp)
+			rk.remove(opp_wins_on)
+			turn_you = list(rk)[0]
+			outcome = 1
+
+		score_round = turn_you + (outcome + 1) * 3
+		# ~ print([turn_opp, turn_you, outcome, score_round])
+
+		score += score_round
+
+	answer = score
 
 	"""<<< Do something here"""
 	utils.print_answer(2, demo, answer)
@@ -90,9 +117,9 @@ def solve_part_2(demo:bool) -> str:
 """
 def main(args):
 
-	solve_part_1(0)
+	# ~ solve_part_1(0)
 
-	# ~ solve_part_2(1)
+	solve_part_2(0)
 
 	return 0
 
