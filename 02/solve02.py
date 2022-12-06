6e6eeb970d96adb12a2058be6b5431b914ba0e88
René Panák <rene.panak@seznam.cz>
2022-12-06 17:13:08 +0100

    solve 02-1 + info 2

--- 02/solve02.py
+++ 02/solve02.py
@@ -9,6 +9,36 @@ import utils
 
 ISSUE=2
 
+def rock_paper_scissors(player1:int, player2:int) -> int:
+	'''
+	in: 1=rock, 2=paper, 3=scissors
+	out: -1=player1 wins, 0=draw, 1=player2 wins
+	'''
+	if player1==player2:
+		return 0
+	rules = {
+		3:2,
+		2:1,
+		1:3
+	}
+	if rules[player1]==player2:
+		return -1
+	else:
+		return 1
+
+def get_round_score(opponent:str, you:str) -> int:
+	score = 0
+	values_opp = " ABC"
+	turn_opp = values_opp.find(opponent)
+	values_you = " XYZ"
+	turn_you = values_you.find(you)
+	if turn_opp>0 and turn_you>0:
+		outcome = rock_paper_scissors(turn_opp, turn_you) # -1,0,1
+		score_round = turn_you + (outcome + 1) * 3
+		# ~ print([turn_opp, turn_you, outcome, score_round])
+		score = score_round
+	return score
+
 """
 	SOLVE PART 1
 """
@@ -18,9 +48,15 @@ def solve_part_1(demo:bool) -> str:
 	print(fn)
 	"""Do something here >>>"""
 
-	print('Part 1 not solved yet')
+	rounds = utils.read_file_into_list(fn, lambda line: tuple(line.strip().split(' ')))
+	# ~ print(rounds)
 
-	answer = None
+	score = 0
+	for gr in rounds:
+		# gr as "game round"
+		score += get_round_score(gr[0], gr[1])
+
+	answer = score
 
 	"""<<< Do something here"""
 	utils.print_answer(1, demo, answer)
@@ -35,7 +71,13 @@ def solve_part_2(demo:bool) -> str:
 	print(fn)
 	"""Do something here >>>"""
 
-	print('Part 2 not solved yet')
+	rounds = utils.read_file_into_list(fn, lambda line: tuple(line.strip().split(' ')))
+	# ~ print(rounds)
+
+	score = 0
+	for gr in rounds:
+		# gr as "game round"
+		score += get_round_score2(gr[0], gr[1])
 
 	answer = None
 
@@ -48,9 +90,9 @@ def solve_part_2(demo:bool) -> str:
 """
 def main(args):
 
-	solve_part_1(1)
+	solve_part_1(0)
 
-	solve_part_2(1)
+	# ~ solve_part_2(1)
 
 	return 0
 
