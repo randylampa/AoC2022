de0aed5ea5d8517701201d0fa3d97f6e31d199b4
René Panák <rene.panak@seznam.cz>
2022-12-06 17:59:58 +0100

    solve 03-1 + info 2

--- 03/solve03.py
+++ 03/solve03.py
@@ -9,6 +9,11 @@ import utils
 
 ISSUE=3
 
+def parse_rucksack_compartments(rucksack:str) -> tuple:
+	rucksack = rucksack.strip()
+	n = len(rucksack)
+	return (rucksack[slice(0,n//2)], rucksack[slice(n//2,n)])
+
 """
 	SOLVE PART 1
 """
@@ -18,9 +23,25 @@ def solve_part_1(demo:bool) -> str:
 	print(fn)
 	"""Do something here >>>"""
 
-	print('Part 1 not solved yet')
+	rucksack_compartments = utils.read_file_into_list(fn, parse_rucksack_compartments)
+	# ~ print(rucksack_compartments)
 
-	answer = None
+	priority_sum = 0
+	for rc in rucksack_compartments:
+		# rc as "rucksack_compartment"
+		common_types = list(set(rc[0]) & set(rc[1]))
+		# ~ print([rc, common_types])
+		for ct in common_types:
+			ord_ct = ord(ct)
+			if ord_ct>90:
+				# should be (a-z)=(1-26)
+				priority = ord_ct-ord('a')+1
+			else:
+				# should be (A-Z)=(27-52)
+				priority = ord_ct-ord('A')+27
+			priority_sum += priority
+
+	answer = priority_sum
 
 	"""<<< Do something here"""
 	utils.print_answer(1, demo, answer)
@@ -48,9 +69,9 @@ def solve_part_2(demo:bool) -> str:
 """
 def main(args):
 
-	solve_part_1(1)
+	solve_part_1(0)
 
-	solve_part_2(1)
+	# ~ solve_part_2(1)
 
 	return 0
 
